60296e0f66e8e33c73f4993355c9e0585905f7d8
jialongwu213961 <jialongwu213961@sohu-inc.com>
2019-02-20 10:52:13 +0800

    add src

---
+++ src/main/java/algo/BinarySearch.java
@@ -0,0 +1,16 @@
+package algo;
+
+/**
+ * @author wu
+ * @date 2019/2/14 11:18
+ * @remark
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ src/main/java/algo/MedianSearch.java
@@ -0,0 +1,9 @@
+package algo;
+
+/**
+ * @author wu
+ * @date 2019/2/14 11:18
+ * @remark
+ */
+public class MedianSearch {
+}
