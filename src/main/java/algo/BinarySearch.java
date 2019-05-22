9623c565794d637ae86e7617f13a9e0798fbf234
巫佳龙(北京研发中心/实习生) <jialongwu@sohu-inc.com>
2019-05-23 01:19:08 +0800

    add 3

--- src/main/java/algo/BinarySearch.java
+++ src/main/java/algo/BinarySearch.java
@@ -1,15 +1,31 @@
 package algo;
 
-/**
+import java.util.concurrent.CountDownLatch;
+
+/** 二分查找
  * @author wu
  * @date 2019/2/14 11:18
  * @remark
  */
 public class BinarySearch {
 
-    public static void main(String[] args) {
 
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int mid = (low + high) >> 1;
+        while (low <= high) {
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                high = mid -1;
+            } else {
+                low = mid + 1;
+            }
+            mid = (low + high) >> 1;
+        }
+        return -1;
     }
+
 
 
 
---
+++ src/main/java/basic/ListNode.java
@@ -0,0 +1,12 @@
+package basic;
+
+/**
+ * Created by 巫佳龙 on 2019/5/22.
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+
+}
